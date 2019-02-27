cb6b6ecd3abe78676a8c0e0e0a570d944d14fc7d
Lch <lch@LchdeMacBook-Pro.local>
2019-02-27 11:39:00 +0800

    dev

--- MyBigHouses/MyBigHouses/settings.py
+++ MyBigHouses/MyBigHouses/settings.py
@@ -55,13 +55,13 @@ ROOT_URLCONF = 'MyBigHouses.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.templates.context_processors.debug',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],

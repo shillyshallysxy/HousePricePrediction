fd7484c990dc8523d5e1bd4a70c3099a3e3eba5c
mahonghui <amisher@163.com>
2019-03-05 18:59:55 +0800

    上传 login_required 装饰器

--- MyBigHouses/utils/login.py
+++ MyBigHouses/utils/login.py
@@ -1,22 +1,5 @@
 # 登录检查装饰器
 from django.http.response import JsonResponse
-from django.
-import json
-import datetime
-
-def object_to_json(self):
-    fields = []
-    for field in self._meta.fields:
-        fields.append(field.name)
-    d = {}
-    for attr in fields:
-        if isinstance(getattr(self, attr), datetime.datetime):
-            d[attr] = getattr(self, attr).strftime('%Y-%m-%d %H:%M:%S')
-        elif isinstance(getattr(self, attr), datetime.date):
-            d[attr] = getattr(self, attr).strftime("%Y-%m-%d")
-        else:
-            d[attr] = getattr(self, attr)
-        return json.dumps(d)
 
 
 def login_required(view_func):
@@ -24,6 +7,5 @@ def login_required(view_func):
         if request.session.has_key('user'):
             return view_func(request, *args, **kwargs)
         else:
-            # return JsonResponse({"code": 1, 'msg': "请您先登录~"})
-            return redirect(reverse("user:login"))
-    return wrapper(view_func)
+            return JsonResponse({"code": 1, 'msg': "请您先登录~"})
+    return wrapper

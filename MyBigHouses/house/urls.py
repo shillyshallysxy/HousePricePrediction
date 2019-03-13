1c1a667e8d9a67480b1bac3f137d0da53e64c137
mahonghui <amisher@163.com>
2019-03-13 16:38:38 +0800

    增加详情视图

--- MyBigHouses/house/urls.py
+++ MyBigHouses/house/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import History, CityInfoView, SubLocationPriceView, HouseOverView
+from .views import History, CityInfoView, SubLocationPriceView, HouseOverView, HouseListFilterView, HouseDetailView
 
 app_name = 'house'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'price/(?P<city>(\w*?))/history', History.as_view(), name="history_price"),
     url(r'city_info/(?P<location>.*)', CityInfoView.as_view(), name="city_info"),
     url(r'price/(?P<city_name>(\w*?))/sub_location', SubLocationPriceView.as_view(), name="sub_price"),
-    url(r'price/(?P<city_name>(\w*?))/overview', HouseOverView.as_view(), name="house_overview")
+    url(r'price/(?P<city_name>(\w*?))/overview', HouseOverView.as_view(), name="house_overview"),
+    url(r'list/(?P<city_name>.*)', HouseListFilterView.as_view(), name="house_list_filter"),
+    url(r'detail/(?P<house_id>\d+)', HouseDetailView.as_view(), name="house_detail")
 ]

d635b32d4f5533f0933b96718fdb863ae5b29460
shillyshallysxy <895484721@qq.com>
2019-03-13 10:34:15 +0800

    新增了首页tips的接口以及首页tips动态变化的更新

--- MyBigHouses/house/urls.py
+++ MyBigHouses/house/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import History, CityInfoView, SubLocationPriceView, HouseOverView
+from .views import History, CityInfoView, SubLocationPriceView, HouseOverView, HouseMainPageView
 
 app_name = 'house'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'price/(?P<city>(\w*?))/history', History.as_view(), name="history_price"),
     url(r'city_info/(?P<location>.*)', CityInfoView.as_view(), name="city_info"),
     url(r'price/(?P<city_name>(\w*?))/sub_location', SubLocationPriceView.as_view(), name="sub_price"),
-    url(r'price/(?P<city_name>(\w*?))/overview', HouseOverView.as_view(), name="house_overview")
+    url(r'price/(?P<city_name>(\w*?))/overview', HouseOverView.as_view(), name="house_overview"),
+    url(r'price/(?P<city_name>(\w*?))/mainpage_overview', HouseMainPageView.as_view(), name="mainpage_overview")
 ]

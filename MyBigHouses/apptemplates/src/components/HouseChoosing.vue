50c783bf3b8d849a55c8373896aa30bc320ae64a
Lch <lch@LchdeMacBook-Pro.local>
2019-03-19 14:22:10 +0800

    看房界面联选

--- MyBigHouses/apptemplates/src/components/HouseChoosing.vue
+++ MyBigHouses/apptemplates/src/components/HouseChoosing.vue
@@ -6,7 +6,7 @@
 					<div class="bar_1">
 						<el-popover placement="bottom" width="200" trigger="click" v-model='price_value'>
 							<el-table :data="price_data" @row-click="show_price_select">
-								<el-table-column width="150" property="price" ></el-table-column>
+								<el-table-column width="150" property="price"></el-table-column>
 							</el-table>
 							<el-button slot="reference" id="show_area" class="bar_1_1">{{price}}</el-button>
 						</el-popover>
@@ -17,7 +17,7 @@
 						</div> -->
 						<el-popover placement="bottom" width="200" trigger="click" v-model='area_value'>
 							<el-table :data="area_data" @row-click="show_area_select">
-								<el-table-column width="150" property="area" ></el-table-column>
+								<el-table-column width="150" property="area"></el-table-column>
 							</el-table>
 							<el-button slot="reference" id="show_area" class="bar_1_1">{{area}}</el-button>
 						</el-popover>
@@ -25,7 +25,7 @@
 					<div class="bar_1">
 						<el-popover placement="bottom" width="200" trigger="click" v-model='house_value'>
 							<el-table :data="house_data" @row-click="show_house_select">
-								<el-table-column width="150" property="house" ></el-table-column>
+								<el-table-column width="150" property="house"></el-table-column>
 							</el-table>
 							<el-button slot="reference" id="show_area" class="bar_1_1">{{house}}</el-button>
 						</el-popover>
@@ -33,14 +33,21 @@
 					<div class="bar_1">
 						<el-popover placement="bottom" width="200" trigger="click" v-model='region_value'>
 							<el-table :data="region_data" @row-click="show_region_select">
-								<el-table-column width="150" property="region" ></el-table-column>
+								<el-table-column width="150" property="region"></el-table-column>
 							</el-table>
 							<el-button slot="reference" id="show_area" class="bar_1_1">{{region}}</el-button>
 						</el-popover>
 					</div>
 					<div class="bar_2">
+
 						<p class="bar_2_1">标签：</p>
-						<div class="bar_2_2">
+						<el-checkbox v-model="underground" label="近地铁" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="five_year" label="满五年" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="decoration" label="精装修" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="price_low" label="总价低" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="watch" label="随时看" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="south_north" label="南北向" border class="bar_2_2"></el-checkbox>
+						<!-- <div class="bar_2_2">
 							<p class="bar_2_5">近地铁</p>
 						</div>
 						<div class="bar_2_2">
@@ -61,7 +68,8 @@
 						<div class="bar_2_3">
 							<p class="bar_2_5">南北向</p>
 						</div>
-
+						<el-checkbox v-model="checked3" label="备选项1" border class="bar_2_2"></el-checkbox>
+						<el-checkbox v-model="checked4" label="备选项2" border class="bar_2_2"></el-checkbox> -->
 						<div class="bar_2_4">
 							<p class="bar_2_5" style="margin-top: 6px;color: white;">
 								筛选
@@ -76,32 +84,48 @@
 				<div class="tips">
 					<div>
 						<div class="tip">
-							<p class="tip_1">默认</p>
+							<el-button slot="reference" id="show_area" style="border: 0px;height: 33px;background-color: rgb(249, 249, 249);" @click="default_click">默认</el-button>
 						</div>
 						<div class="tip">
-							<p class="tip_1">单价</p>
+							<el-popover placement="bottom"  trigger="click" v-model='single_price_value'>
+								<el-table :data="single_price_data" @row-click="show_single_price_select">
+									<el-table-column width="150" property="single_price"></el-table-column>
+								</el-table>
+								<el-button slot="reference" id="show_single_price" style="border: 0px;height: 33px;width: 70px;background-color: rgb(249, 249, 249);">单价</el-button>
+							</el-popover>
 							<div class="tip2">
 								<i class="el-icon-caret-top"></i>
 							</div>
-							<div class="tip2" style="margin-top: 0px;">
+							<div class="tip2" style="margin-top: -20px;">
+								<i class="el-icon-caret-bottom"></i>
+							</div>
+						</div>
+						
+						<div class="tip">
+							<el-popover placement="bottom"  trigger="click" v-model='total_price_value'>
+								<el-table :data="total_price_data" @row-click="show_total_price_select">
+									<el-table-column width="150" property="total_price"></el-table-column>
+								</el-table>
+								<el-button slot="reference" id="show_total_price" style="border: 0px;height: 33px;width: 70px;background-color: rgb(249, 249, 249);">总价</el-button>
+							</el-popover>
+							<div class="tip2">
+								<i class="el-icon-caret-top"></i>
+							</div>
+							<div class="tip2" style="margin-top: -20px;">
 								<i class="el-icon-caret-bottom"></i>
 							</div>
 						</div>
 						<div class="tip">
-							<p class="tip_1">总价</p>
+							<el-popover placement="bottom"  trigger="click" v-model='total_area_value'>
+								<el-table :data="total_area_data" @row-click="show_total_area_select">
+									<el-table-column width="150" property="total_area"></el-table-column>
+								</el-table>
+								<el-button slot="reference" id="show_total_area" style="border: 0px;height: 33px;width: 70px;background-color: rgb(249, 249, 249);">面积</el-button>
+							</el-popover>
 							<div class="tip2">
 								<i class="el-icon-caret-top"></i>
 							</div>
-							<div class="tip2" style="margin-top: 0px;">
-								<i class="el-icon-caret-bottom"></i>
-							</div>
-						</div>
-						<div class="tip">
-							<p class="tip_1">面积</p>
-							<div class="tip2">
-								<i class="el-icon-caret-top"></i>
-							</div>
-							<div class="tip2" style="margin-top: 0px;">
+							<div class="tip2" style="margin-top: -20px;">
 								<i class="el-icon-caret-bottom"></i>
 							</div>
 						</div>
@@ -147,86 +171,156 @@
 				price_value: false,
 				price: '选择价格',
 				price_data: [{
-						price: '不限',
-					}, {
-						price: '0～60w'
-					}, {
-						price: '60～100w'
-					}, {
-						price: '100～150w'
-					}, {
-						price: '150～200w'
-					}, {
-						price: '200～300w'
-					}, {
-						price: '300～500w'
-					}, {
-						price: '500w以上'
-					}
-				],
+					price: '不限',
+				}, {
+					price: '0～60w'
+				}, {
+					price: '60～100w'
+				}, {
+					price: '100～150w'
+				}, {
+					price: '150～200w'
+				}, {
+					price: '200～300w'
+				}, {
+					price: '300～500w'
+				}, {
+					price: '500w以上'
+				}],
 				house_value: false,
-				house:"选择户型",
-				house_data:[{
-					house:'居室不限'
-				},{
-					house:'一室'
-				},{
-					house:'两室'
-				},{
-					house:'三室'
-				},{
-					house:'四室'
-				},{
-					house:'五室'
-				},{
-					house:'五室以上'
+				house: "选择户型",
+				house_data: [{
+					house: '居室不限'
+				}, {
+					house: '一室'
+				}, {
+					house: '两室'
+				}, {
+					house: '三室'
+				}, {
+					house: '四室'
+				}, {
+					house: '五室'
+				}, {
+					house: '五室以上'
 				}],
 				region_value: false,
 				region: "选择地区",
-				region_data:[],
+				region_data: [],
+				
+				underground: false,
+				five_year:false,
+				decoration:false,
+				price_low:false,
+				watch:false,
+				south_north:false,
+				
+				single_price_value: false,
+				single_price:'单价',
+				single_price_data:[
+					{
+						single_price:'从高到低',
+					},{
+						single_price:"从低到高"
+					}
+				],
+				total_price_value: false,
+				total_price:'总价',
+				total_price_data:[
+					{
+						total_price:'从高到低',
+					},{
+						total_price:"从低到高"
+					}
+				],
+				total_area_value: false,
+				total_area:'面积',
+				total_area_data:[
+					{
+						total_area:'从高到低',
+					},{
+						total_area:"从低到高"
+					}
+				],
 			};
 		},
 		mounted() {
 			this.set_region()
 		},
 		methods: {
-			set_region(){
+			set_region() {
 				let temp_region = this.get_region
-				for(var i=0;i<temp_region.length;i++){
-					var temp={}
+				for (var i = 0; i < temp_region.length; i++) {
+					var temp = {}
 					temp['region'] = temp_region[i]
 					this.region_data.push(temp)
 				}
-				
+
 			},
 			show_area_select(row) {
 				this.area = row.area
 				this.area_value = false
 			},
-			show_price_select(row){
+			show_price_select(row) {
 				this.price = row.price
 				this.price_value = false
 			},
-			show_house_select(row){
+			show_house_select(row) {
 				this.house = row.house
 				this.house_value = false
 			},
-			show_region_select(row){
+			show_region_select(row) {
 				this.region = row.region
 				this.region_value = false
+			},
+			show_single_price_select(row){
+				this.single_price = row.single_price
+				this.single_price_value =false
+			},
+			show_total_price_select(row){
+				this.total_price = row.total_price
+				this.total_price_value =false
+			},
+			show_total_area_select(row){
+				this.total_area = row.total_area
+				this.total_area_value =false
+			},
+			default_click(){
+				
+			},
+			get_data(){
+				var self = this
+				self.$ajax({
+					url:global_.IpUrl,
+					methods:'post',
+					data:{
+						price_selcet: self.price,
+						area_select: self.area,
+						house_select:self.house,
+						region_select: self.region,
+						underground: self.underground,
+						five_year:self.five_year,
+						decoration:self.decoration,
+						price_low:self.price_low,
+						watch:self.watch,
+						south_north:self.south_north,
+					}
+				})
 			}
+			
+			
 		},
-		computed:{
-			get_region(){
+		computed: {
+			get_region() {
 				var temp_region = this.get_city
 				var arr = this.get_city.split('')
-				if(arr[arr.length-1]=="市"){
-					arr.splice(arr.length-1,1)
+				if (arr[arr.length - 1] == "市") {
+					arr.splice(arr.length - 1, 1)
 					temp_region = arr.join('')
 				}
 				return global_.city_region_mapping[temp_region]
 			},
-			get_city(){
+			get_city() {
 				return store.state.area.city
 			}
 		}
@@ -363,10 +457,9 @@
 
 	.bar_2_2 {
 		/*未选中*/
-		width: 60px;
+		width: 70px;
 		height: 25px;
 		background-color: rgb(226, 226, 226);
-		margin-left: 10px;
 		margin-top: 12px;
 		float: left;
 		border-radius: 5px;
